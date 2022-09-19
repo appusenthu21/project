216d7e4527a623a67eb1531dca434a74a90b5c88
appusenthu21 <senthamizhan2195@gmail.com>
2022-09-19 07:16:00 -0700

    message

---
+++ src/test/java/Sample1.java
@@ -0,0 +1,20 @@
+
+public class Sample1 {
+private void m1() {
+	System.out.println("m1");
+
+}
+private void m2() {
+	System.out.println("m2");
+	
+
+}
+private void m3() {
+	System.out.println("m3");
+
+}
+private void m4() {
+	System.out.println("m4");
+
+}
+}
