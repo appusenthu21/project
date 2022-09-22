d955cd1ffa0fda1a6c44acadb6cc9b34c8f0f319
Mustafa@9719 <mustafaofficial123@gmail.com>
2022-09-22 12:50:53 +0530

    3methods created

--- src/test/java/Sample1.java
+++ src/test/java/Sample1.java
@@ -20,4 +20,15 @@ private void m4() {
 public void m5() {
 	System.out.println("m5");
 }
+private void m6() {
+	System.out.println("m6");
+
+}
+private void m7() {
+	System.out.println("m47");
+
+}
+public void m8() {
+	System.out.println("m8");
+}
 }
